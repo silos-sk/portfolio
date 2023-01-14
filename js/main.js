f59a65f7b5ddb0f40f7783c9222ce180c6bd9741
Shaira <sil21019@byui.edu>
2023-01-14 23:23:42 +0000

    porfolio items converted to json

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+import { getApi, renderPortfolio } from "./utilities.js";
+
 const barIcon = document.querySelector("#bar-icon");
 const navBar = document.querySelector("nav");
 const xIcon = document.querySelector("#x-icon");
-// const portfolioImg = document.querySelector(".portfolio-img");
-// const card = document.querySelector(".card")
 
 barIcon.addEventListener("click", () => {
   barIcon.classList.add("hide");
@@ -15,3 +15,7 @@ xIcon.addEventListener("click", ()=>{
     barIcon.classList.remove("hide")
     navBar.classList.remove("active");
 });
+
+getApi("./js/portfolio.json", "portfolio", renderPortfolio);
+renderPortfolio(portfolio, main);
+
